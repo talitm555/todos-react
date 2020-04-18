1ecd41e630210ffb954693b7897a4cfc31d4c497
Talit Mahmood <talitm555@gmail.com>
2020-04-18 16:20:30 +0500

    Minor changes in about.js and package.json

--- src/components/pages/About.js
+++ src/components/pages/About.js
@@ -3,7 +3,7 @@ import React from 'react'
 export default function About() {
     return (
         <React.Fragment>
-            <p>This is about page.</p>
+            <p>This is about page of Todo App created using React by Talit Mahmood.</p>
         </React.Fragment>
     )
 }
